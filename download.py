51b942921ecb8c720f7ca2153ea8de997cdf2721
ZhangZheng <772125261@qq.com>
2018-10-11 10:17:01 +0800

    a little change in 'try' module

--- download.py
+++ download.py
@@ -110,9 +110,13 @@ class DownloadFTP(object):
             if self.config.read_config_part(prodcut):
                 print('%s downloading......' % self.config.product)
                 self._login_ftp()
-                self._download_product()
+                try:
+                    self._download_product()
+                except FileNotFoundError:
+                    print('file not found')
+                else:
+                    print('success')
                 self._quit_ftp()
-                print('success')
 
     def _download_product(self):
         if self.config.product == 'sp3' or self.config.product == 'clk':
@@ -153,18 +157,14 @@ class DownloadFTP(object):
             product_name = 'CAS0MGXRAP_%d%03d0000_01D_01D_DCB.BSX.gz' % (
                 self.config.date.year, self.config.date.timetuple().tm_yday)
             self._download_file(self.session, product_name, self.config.dest)
-            try:
-                extractDCBFromSNX.extractDCBFromSNX(os.path.join(self.config.dest, product_name).replace('.gz', ''),
-                                                    self.config.dest, True)
-            except FileNotFoundError:
-                print('No such file: %s' % os.path.join(self.config.dest, product_name).replace('.gz', ''))
-            else:
-                for i in ['C1', 'P2', 'P3']:
-                    old_file = os.path.join(self.config.dest, 'P1%s%02d%02d%02d.DCB' % (
-                        i, self.config.date.year % 100, self.config.date.month, self.config.date.day))
-                    new_file = os.path.join(os.path.split(self.config.dest)[0],
-                                            'P1%s%02d%02d.DCB' % (i, self.config.date.year % 100, self.config.date.month))
-                    copy_file(old_file, new_file)
+            extractDCBFromSNX.extractDCBFromSNX(os.path.join(self.config.dest, product_name).replace('.gz', ''),
+                                                self.config.dest, True)
+            for i in ['C1', 'P2', 'P3']:
+                old_file = os.path.join(self.config.dest, 'P1%s%02d%02d%02d.DCB' % (
+                    i, self.config.date.year % 100, self.config.date.month, self.config.date.day))
+                new_file = os.path.join(os.path.split(self.config.dest)[0],
+                                        'P1%s%02d%02d.DCB' % (i, self.config.date.year % 100, self.config.date.month))
+                copy_file(old_file, new_file)
         elif self.config.product == 'brdm':
             self.config.dest = os.path.join(self.config.dest, '%d' % self.config.date.year)
             if not os.path.isdir(self.config.dest):
